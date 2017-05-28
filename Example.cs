7693124a3a9434b95f232d827b2ec4988f606b90
PassiveModding <passivemodding@gmail.com>
2017-05-28 22:38:31 +1000

    Example uses of each of the preconditions

---
+++ Example.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+using PassiveBOT.preconditions;
+
+namespace PassiveBOT.Commands
+{
+    public class Example : ModuleBase
+    {
+        [Command("rip")]
+        [RequireChannel("memes")]
+        public async Task Rip()
+            =>await ReplyAsync("This command will only work in channels titled `memes` or starting with `memes-`");
+
+        [Command("rekt")]
+        [BanChannel("announcements")]
+        public async Task Rekt()
+            =>await ReplyAsync("this command will not work in any channel titled `announcements` or starting with `announcements-`");
+
+        [Command("idk")]
+        [RequireTopic("info")]
+        public async Task Idk()
+            =>await ReplyAsync("this command requires you include `[info]` in the topic of your channel");
+
+        [Command("gg")]
+        [BanTopic("help")]
+        public async Task Gg() 
+            =>await ReplyAsync("this command will not work in any channel with `[help]` in its topic");
+    }
+}
